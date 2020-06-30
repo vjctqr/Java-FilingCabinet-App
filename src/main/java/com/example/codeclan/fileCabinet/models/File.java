6a8c780c6d343b5b1a26dce09bbcfdf3654cf7c0
Victor <victorsetlhare@gmail.com>
2020-06-30 17:39:39 +0100

    file methods

--- src/main/java/com/example/codeclan/fileCabinet/models/File.java
+++ src/main/java/com/example/codeclan/fileCabinet/models/File.java
@@ -19,6 +19,42 @@ public class File {
     @Column(name = "size")
     private int size;
 
+    @ManyToOne
+    @JoinColumns(name ="folder_id", nullable = false)
+    private Folder folder;
 
+    public File(){
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Folder getFolder(){
+        this.size = size;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
--- src/main/java/com/example/codeclan/fileCabinet/models/Folder.java
+++ src/main/java/com/example/codeclan/fileCabinet/models/Folder.java
@@ -1,4 +1,9 @@
 package com.example.codeclan.fileCabinet.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name)
 public class Folder {
 }
