4933b451075d8dd43273f00b9f09f45f09bad16d
Victor <victorsetlhare@gmail.com>
2020-06-30 17:02:43 +0100

    file class construct

--- src/main/java/com/example/codeclan/fileCabinet/models/File.java
+++ src/main/java/com/example/codeclan/fileCabinet/models/File.java
@@ -1,4 +1,24 @@
 package com.example.codeclan.fileCabinet.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
 public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "extension")
+    private String extension;
+
+    @Column(name = "size")
+    private int size;
+
+
+
 }
